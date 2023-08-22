b785b0eeab0f531289c1e6f4cb4ca6b37f084242
Katharinaad <vermilion.adams@gmail.com>
2023-08-22 14:52:58 +0200

    Create serviceImages.js

---
+++ src/js/serviceImages.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+// Unique KEY & BSE_URL
+const API_KEY = '38980097-7acd167c88be026b0eb497bb1';
+const BASE_URL = 'https://pixabay.com/api/';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+// function serviceImages(value) {
+//   const params = new URLSearchParams({
+//     key: API_KEY,
+//     q: value,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//   });
+
+//   return fetch(`${BASE_URL}?${params}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.statusText);
+//     }
+//     return response.json();
+//   });
+// }
+
+// serviceImages();
+
+const serviceImages = async value => {
+  const { data } = await axios({
+    params: {
+      key: KEY,
+      q: value,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
+  return data;
+};
+
+export { serviceImages };
