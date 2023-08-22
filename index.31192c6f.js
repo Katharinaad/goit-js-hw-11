4c1bf841524aa304fd25f2805f47831d62fd4992
Katharinaad <127932159+Katharinaad@users.noreply.github.com>
2023-08-22 16:48:49 +0000

    Deploying to gh-pages from @ Katharinaad/goit-js-hw-11@8b8fa1f91b93bc07ea234cf7a64ff0645d790051 🚀

---
+++ index.31192c6f.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".search-form");document.querySelector(".gallery"),e.elements[0];e.addEventListener("submit",(function(){}))}();
+//# sourceMappingURL=index.31192c6f.js.map
---
+++ index.5c863813.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".search-form");document.querySelector(".gallery"),e.elements[0];e.addEventListener("submit",(function(){}));
+//# sourceMappingURL=index.5c863813.js.map
